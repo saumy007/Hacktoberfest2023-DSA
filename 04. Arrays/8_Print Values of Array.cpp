236af8f85d2bacde94ab6301421c67f1eb56e0d8
Saumy Sharma <saumysharma007@gmail.com>
2023-10-03 01:23:24 +0530

    all arrays printed

---
+++ 04. Arrays/8_Print Values of Array.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[6]={1,2,3,4,4,6};// declaring array
+    for(int i = 0; i< 0; i++){
+        cout << arr[i];// this will print array
+    }
+
+} 
